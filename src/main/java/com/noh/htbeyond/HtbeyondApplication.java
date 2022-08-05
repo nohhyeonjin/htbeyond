926ebab3a4b201afff3ac634d8e60f9c8884a917
Noh Hyeon Jin <hjforj@naver.com>
2022-08-05 12:01:18 +0900

    initial commit

---
+++ src/main/java/com/noh/htbeyond/HtbeyondApplication.java
@@ -0,0 +1,13 @@
+package com.noh.htbeyond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtbeyondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtbeyondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noh/htbeyond/HtbeyondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noh.htbeyond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtbeyondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
